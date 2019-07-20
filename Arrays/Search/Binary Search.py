9d858ade26c13e34a3c84bb6bddb48c319de722e
vprasad45 <vprasad46@gmail.com>
2019-07-20 14:04:18 +0530

    Searching Algorithms for Arrays: Binary Search

---
+++ Arrays/Search/Binary Search.py
@@ -0,0 +1,16 @@
+
+# Search Algorithm for sorted arrays: Binary Search
+
+def search(arr, start, end, searchElement):
+    middle = int((start + end) / 2)
+    if end > 0 and middle < len(arr):
+        if arr[middle] == searchElement:
+            return middle
+        elif arr[middle] < searchElement:
+            return search(arr, middle + 1, end, searchElement)
+        elif arr[middle] > searchElement:
+            return search(arr, start, middle - 1, searchElement)
+
+
+arr = [1, 2, 3, 4, 5]
+print("The value is found at index: " + str(search(arr, 0, len(arr), 3)))
